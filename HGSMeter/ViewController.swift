766dee8dec6740e24a522736a510286d5f01ea41
Hiren Gohil <hirengohil.spaceo@gmail.com>
2020-04-13 12:02:54 +0530

    Example

--- HGSMeter/ViewController.swift
+++ HGSMeter/ViewController.swift
@@ -7,18 +7,48 @@
 //
 
 import UIKit
+import HGSMeter
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var vwMeter: HGSMeterClass!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        DispatchQueue.main.async {
+            let vw : HGSMeterClass?
+            self.vwMeter.seperatorColor = UIColor.darkGray
+            self.vwMeter.defaultColorDarkness = 80
+            self.vwMeter.colorSections = [(color: UIColor.green, lines: 21),
+                                          (color: UIColor.yellow, lines: 7),
+                                          (color: UIColor.blue, lines: 5),
+                                          (color: UIColor.red, lines: 3)]
+            
+          
+            self.vwMeter.currentLevel = 60
+            self.vwMeter.autoFlush = false
+            self.vwMeter.setUpView()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func btnRandomTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0, animations: {
+            let rand = Int.random(in: 0..<self.vwMeter.maxValue)
+            print("rand: ",rand)
+            self.vwMeter.currentLevel = rand
+        }) { (success) in
+            self.vwMeter.flushMeter()
+        }
+    }
+    
 }
 
